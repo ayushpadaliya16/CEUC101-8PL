efc0e21979bae72098d1e870e307d28968fedc80
ayushpadaliya16 <ayushpadaliya161@gmail.com>
2025-11-15 22:01:21 +0530

    Refactor code with comments and formatting
    Added comments for clarity and updated formatting.

--- pra19_3.c
+++ pra19_3.c
@@ -1,3 +1,8 @@
+/*aim: To make Patterns
+editor: Ayush padaliya
+last updated date:25/08/2025
+*/
+
 #include <stdio.h>
 
 void main()
@@ -42,3 +47,4 @@ void main()
         printf("\n");
     }
 }
+
